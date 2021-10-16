58529bc340f4d7077230d71414343d3c255d6692
Anubhab Chakraborty <anubhab18@iiserb.ac.in>
2021-10-16 14:02:52 +0530

    added more tests

--- pyimage/preprocessing.py
+++ pyimage/preprocessing.py
@@ -1,13 +1,27 @@
 import numpy as np
 from skimage import io
+from skimage import color
+from skimage.color.colorconv import rgb2gray
 
-class Image():
+class ImageProcessor():
     
     def __init__(self) -> None:
         self.image = None
+        # setting a sample image for default path
+        self.path = "../assets/purple_ocimum_basilicum.png"
     
-    def image_import(self, path=None):
+    def load_image(self, path=None):
         if path is None:
             path = self.path
         self.image = io.imread(path)
         
+    def to_gray(self):
+        self.image = rgb2gray(self.image)
+
+    def show_image(self):
+        if self.image is None:
+            self.load_image()
+        self.to_gray()
+        io.imshow(self.image)
+        io.show()
+        return True
--- test/test_preprocessing.py
+++ test/test_preprocessing.py
@@ -1,6 +1,17 @@
-from pyimage.preprocessing import Image
+from pyimage.preprocessing import ImageProcessor
 
-def test_image_import():
-    image = Image()
-    image.image_import()
-    assert image is not None
+def test_load_image():
+    image_processor = ImageProcessor()
+    image_processor.load_image()
+    assert image_processor.image is not None
+
+def test_to_gray():
+    image_processor = ImageProcessor()
+    image_processor.load_image()
+    image_processor.to_gray()
+    # converts a 3 dimensional array(length, width & channel) to 2 dimensional array (length, width)
+    assert len(image_processor.image.shape) is 2
+
+def test_show_image():
+    image_processor = ImageProcessor()
+    assert image_processor.show_image()
