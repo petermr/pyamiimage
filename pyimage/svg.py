53bc62b6646f26dc30c9ea70bc4aa48ef65f3053
petermr <peter.murray.rust@googlemail.com>
2021-11-09 15:58:19 +0000

    added and tested bounding boxes

--- pyimage/svg.py
+++ pyimage/svg.py
@@ -57,8 +57,10 @@ class SVGShape(AbsSVG):
 class SVGRect(AbsSVG):
     TAG = "rect"
 
-    def __init__(self):
+    def __init__(self, bbox=None):
         super().__init__(self.TAG)
+        if bbox is not None:
+            self.set_bbox(bbox)
 
     def set_height(self, h):
         self.set_float_attribute("height", h)
@@ -68,6 +70,12 @@ class SVGRect(AbsSVG):
 
     def set_xy(self, tuple2):
         assert len(tuple2) == 2
+        self.set_float_attribute("x", tuple2[0])
+        self.set_float_attribute("y", tuple2[1])
+
+    def set_bbox(self, bbox_tuple):
+        assert bbox_tuple is not None
+        assert len(bbox_tuple) == 2
         self.set_float_attribute("x", tuple2[0])
         self.set_float_attribute("y", tuple2[1])
 
@@ -132,6 +140,101 @@ class SVGTextBox(SVGG):
             assert type(svg_shape) is SVGShape
             self.svgg.append(svg_shape)
 
+class Bbox:
+    """bounding box tuple2 of tuple2s
+    """
+    def __init__(self, tuple22=None):
+        """
+        Must have a valid bbox
+        :param tuple22:
+        """
+        self.tuple22 = [(), ()]
+        if tuple22 is not None:
+            self.set_tuples(tuple22)
+
+    def set_tuples(self, tuple22):
+        if tuple22 is None:
+            raise ValueError("no tuples given")
+        if len(tuple22) != 2:
+            raise ValueError("must be 2 tuples of tuples")
+        if len(tuple22[0]) != 2 or len(tuple22[1]) != 2:
+            raise ValueError("each child tuple must be a 2-tuple")
+        self.set_xrange(tuple22[0])
+        self.set_yrange(tuple22[1])
+
+    def set_xrange(self, tuple2):
+        self.set_tuple(0, tuple2)
+
+    def get_xrange(self):
+        return self.tuple22[0]
+
+    def get_width(self):
+        return self.get_xrange()[1] - self.get_xrange()[0] if len(self.get_xrange()) == 2 else None
+
+    def set_yrange(self, tuple2):
+        self.set_tuple(1, tuple2)
+
+    def get_yrange(self):
+        return self.tuple22[1]
+
+    def get_height(self):
+        return self.get_yrange()[1] - self.get_yrange()[0] if len(self.get_yrange()) == 2 else None
+
+    def set_tuple(self, index, tuple2):
+        if index != 0 and index != 1:
+            raise ValueError(f"bad tuple index {index}")
+        val0 = float(tuple2[0])
+        val1 = float(tuple2[1])
+        if val1 < val0:
+            raise ValueError(f"ranges must be increasing {val0} !<= {val1}")
+        self.tuple22[index] = (val0, val1)
+
+    def __str__(self):
+        return str(self.tuple22)
+
+    def intersect(self, bbox):
+        """
+        inclusive intersection of boxes (AND)
+        if any fields are empty returns None
+        :param bbox:
+        :return: new Bbox (max(min) ... (min(max)) or None if any  errors
+        """
+        bbox1 = None
+        if bbox is not None:
+            xrange = self.intersect_range(self.get_xrange(), bbox.get_xrange())
+            yrange = self.intersect_range(self.get_yrange(), bbox.get_yrange())
+            bbox1 = Bbox((xrange, yrange))
+        return bbox1
+
+    def union(self, bbox):
+        """
+        inclusive merging of boxes (OR)
+        if any fields are empty returns None
+        :param bbox:
+        :return: new Bbox (min(min) ... (max(max)) or None if any  errors
+        """
+        bbox1 = None
+        if bbox is not None:
+            xrange = self.union_range(self.get_xrange(), bbox.get_xrange())
+            yrange = self.union_range(self.get_yrange(), bbox.get_yrange())
+            bbox1 = Bbox((xrange, yrange))
+        return bbox1
+
+    def intersect_range(self, range0, range1):
+        """intersects 2 range tuples"""
+        range = ()
+        print(range0, range1)
+        if len(range0) == 2 and len(range1) == 2:
+            range = (max(range0[0], range1[0]), min(range0[1], range1[1]))
+        return range
+
+
+"""If you looking for the overlap between two real-valued bounded intervals, then this is quite nice:
+
+def overlap(start1, end1, start2, end2):
+    how much does the range (start1, end1) overlap with (start2, end2)
+    return max(max((end2-start1), 0) - max((end2-end1), 0) - max((start2-start1), 0), 0)
+I couldn't find this online anywhere so I came up with this and I'm posting here."""
 
 class AmiArrow(SVGG):
     """
--- test/test_svg.py
+++ test/test_svg.py
@@ -1,7 +1,21 @@
 
-from pyimage.svg import SVGRect, SVGTitle, SVGText, SVGTextBox, SVGG, SVGSVG
+from pyimage.svg import SVGRect, SVGTitle, SVGText, SVGTextBox, SVGG, SVGSVG, Bbox
 
 class TestSVG():
+
+    def test_good_attribute(self):
+        rect = SVGRect()
+        rect.element.attrib["foo"] = "bar"
+        assert rect.tostring() == """<svg:rect xmlns:svg="http://www.w3.org/2000/svg" foo="bar"/>
+"""
+
+    def test_bad_attribute(self):
+        rect = SVGRect()
+        try:
+            rect.element.attrib["123"] = "bar"
+            assert True, "should throw bad att name"
+        except ValueError as e:
+            assert str(e) == """Invalid attribute name '123'"""
 
     def test_create_empty_rect(self):
         svg_rect = SVGRect()
@@ -38,5 +52,58 @@ class TestSVG():
   <svg:rect height="50.0" width="100.0" x="200.0" y="300.0"/>
 </svg:svg>
 """
+
+# Bounding box Tests
+
+    def test_create_empty_bbox(self):
+        bbox = Bbox()
+        assert bbox.__str__() == "[(), ()]"
+
+    def test_create_bbox(self):
+        bbox = Bbox(((100,200), (300,400)))
+        assert bbox.__str__() == "[(100.0, 200.0), (300.0, 400.0)]"
+
+    def test_create_bad_bbox(self):
+        try:
+            Bbox(((100,50), (300,400)))
+        except ValueError as e:
+            assert str(e) == "ranges must be increasing 100.0 !<= 50.0"
+
+    def test_update_bbox(self):
+        bbox = Bbox(((100,200), (300,400)))
+        bbox.set_xrange((10,20))
+        assert bbox.__str__() == "[(10.0, 20.0), (300.0, 400.0)]"
+
+    def test_update_bbox(self):
+        bbox = Bbox()
+        assert bbox.__str__() == "[(), ()]"
+        bbox.set_xrange((10,20))
+        assert bbox.__str__() == "[(10.0, 20.0), ()]"
+        bbox.set_yrange((30,40))
+        assert bbox.__str__() == "[(10.0, 20.0), (30.0, 40.0)]"
+
+    def test_get_values(self):
+        bbox = Bbox()
+        assert bbox.get_width() is None
+        assert bbox.get_height() is None
+        assert bbox.get_xrange() is ()
+        assert bbox.get_yrange() is ()
+        bbox.set_xrange((10, 20))
+        assert type(bbox.get_xrange()) is tuple
+        assert bbox.get_xrange() == (10., 20.)
+        assert bbox.get_width() == 10.
+        assert bbox.get_yrange() == ()
+        assert bbox.get_height() is None
+        bbox.set_yrange((30, 70))
+        assert bbox.get_xrange() == (10., 20.)
+        assert bbox.get_width() == 10.
+        assert bbox.get_yrange() == (30., 70.)
+        assert bbox.get_height() == 40.
+
+    def test_get_intersections(self):
+        bbox0 = Bbox(((10,20), (30,40)))
+        bbox1 = Bbox(((13,28), (27,38)))
+        bbox01 = bbox0.intersect(bbox1)
+        assert bbox01.tuple22 == [(13.,20.), (30.,38.)]
 
 
