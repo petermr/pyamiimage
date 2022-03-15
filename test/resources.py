0d1122bbd0f541c4836abe4b12e0257bd7107f95
Anubhab Chakraborty <anuv.chakrabo@gmail.com>
2022-03-15 18:32:58 +0530

    added mex_xrd image

--- test/resources.py
+++ test/resources.py
@@ -90,6 +90,8 @@ class Resources:
     # https://europepmc.org/article/MED/34909142#figures-and-tables
     MED_34909142_3_RAW = Path(TEST_RESOURCE_DIR, "MED_34909142_3.jpeg")
     assert MED_34909142_3_RAW.exists(), f"file exists {MED_34909142_3_RAW}"
+    MED_XRD_FIG_A_RAW = Path(TEST_RESOURCE_DIR, "MED_34909142_3_figA.jpeg")
+    assert MED_XRD_FIG_A_RAW.exists(), f"file {MED_XRD_FIG_A_RAW} doesn't exist"
 
     BATTERY_DIR = Path(TEST_RESOURCE_DIR, "battery")
     BATTERY1_RAW = Path(BATTERY_DIR, "capacity_r_g_b.png")
